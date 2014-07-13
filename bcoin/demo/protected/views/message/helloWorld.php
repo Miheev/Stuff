428d2b885ecdac889d76882094e49369b78fa0ed
Miheev Sergey <serega.khv@gmail.com>
2014-07-14 00:41:19 +1100

    Git Recovery

--- bcoin/demo/protected/views/message/helloWorld.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->breadcrumbs=array(
-	'Message'=>array('message/index'),
-	'HelloWorld',
-);?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-<p><?php echo $time; ?></p>
-<p>You may change the content of this page by modifying the file <tt><?php echo __FILE__; ?></tt>.</p>
