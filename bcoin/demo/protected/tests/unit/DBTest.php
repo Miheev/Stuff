a90e22a04203061e3d4f1d3a37fdf60d550614c5
Miheev Sergey <serega.khv@gmail.com>
2014-07-14 00:42:15 +1100

    Git Recovery

---
+++ bcoin/demo/protected/tests/unit/DBTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: storm
+ * Date: 7/11/14
+ * Time: 3:17 PM
+ */
+
+
+class DBTest extends CTestCase {
+    public function testConnection() {
+        $this->assertTrue(true);
+    }
+
+} 
---
+++ bcoin/trackstar/protected/views/users/_search.php
@@ -0,0 +1,55 @@
+<?php
+/* @var $this UsersController */
+/* @var $model Users */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_login'); ?>
+		<?php echo $form->textField($model,'user_login',array('size'=>15,'maxlength'=>15)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_pass'); ?>
+		<?php echo $form->textField($model,'user_pass',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_name'); ?>
+		<?php echo $form->textField($model,'user_name',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_email'); ?>
+		<?php echo $form->textField($model,'user_email',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_phone'); ?>
+		<?php echo $form->textField($model,'user_phone',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_company'); ?>
+		<?php echo $form->textField($model,'user_company',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
