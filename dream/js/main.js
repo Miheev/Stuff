e0d1a9ae64e607d4ea85a8713091f879a47ad9f2
Miheev Sergey <serega.khv@gmail.com>
2014-07-15 02:06:21 +1100

    Dream: usual page End

--- dream/js/main.js
+++ dream/js/main.js
@@ -5,6 +5,65 @@ $(document).ready(function(){
     $('footer .right .right select').chosen({disable_search_threshold: 10, width: '140px'});
     $('header.header .user .personal select').chosen({disable_search_threshold: 10, width: '100px'});
 
+    if ($('body.dreams').length) {
+        $("div.range-1").noUiSlider({
+            start: [0, 100],
+            step: 5,
+            margin: 20,
+            connect: true,
+            behaviour: 'tap-drag',
+            range: {
+                'min': 0,
+                'max': 100
+            },
+            serialization: {
+                lower: [
+                    $.Link({
+                        target: $(".range-in")
+                    })
+                ],
+                upper: [
+                    $.Link({
+                        target: $(".range-out")
+                    })
+                ],
+                format: {
+                    mark: '%',
+                    decimals: 0
+                }
+            }
+        });
+
+        $("input[name='dream']").kalypto();
+
+
+//            var offset = $("#fixblock").offset(); // запоминаем первоначальные отсупы
+//            $(window).scroll(function() { // во время скроллинга
+//                if ($(window).scrollTop() > offset.top) { // Если скроллинг больше первоначальной позиции
+//                    $("#fixblock").stop().animate({marginTop: $(window).scrollTop() - offset.top}); // увиличиваем отступ сверху
+//                }else{
+//                    $("#fixblock").stop().animate({marginTop: 0}); // Иначе отступ нулевой
+//                }
+//            });
+
+        $(window).scroll(function(){
+            var aTop = $('header.header').height() + $('.menu-container').height();
+            var afooter = aTop + $('.main-container').height()*0.72;
+
+            if ($(this).scrollTop()>=aTop){
+                $('#fixblock').addClass('sticky');
+            } else
+                $('#fixblock').removeClass('sticky');
+
+            delta= $(this).scrollTop() - afooter;
+            delta= (delta < 0)? delta: -delta;
+            if ($(this).scrollTop()>=afooter){
+                $('#fixblock').css('top', delta);
+            } else
+                $('#fixblock').css('top', 0);
+        });
+    }
+
     /**
      * Created by storm on 4/13/14. Riverside / Sound Couture /One Ok Rock / Foreground Eclipse / Flaming June -- Muteki no Soldier / coldrain // Aikawa Nanase / Kokia / Afilia Saga / Calafina / Lost Fairy / Man with a mission / Kasahara Yori, Tarantula / Disarmonia Mundi feat Hallelujah Yoko
      */
