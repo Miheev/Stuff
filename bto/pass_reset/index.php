09a9f75305b40ceb9efb200838559ab1405385dc
Miheev Sergey <serega.khv@gmail.com>
2014-07-16 18:34:56 +1100

    BTO: uf8 convertation fixes

--- bto/pass_reset/index.php
+++ bto/pass_reset/index.php
@@ -1,4 +1,6 @@
 <?
+ini_set('session.cookie_secure', 'true');
+ini_set('session.cookie_httponly', 'true');
 session_start();
 header('Content-type: text/html; charset=utf-8');
 if (!empty($_SESSION["appd"])):
@@ -23,14 +25,14 @@ if (isset($_POST["lg"]) && !empty($_POST["lg"])):
 				<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap.css" />
 				<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap-theme.css" />
 				<link type="text/css" rel="stylesheet" href="/handle-style.css" />
-				<title>�����-������</title>
+				<title>Сброс пароля</title>
 				<style>td { height: 40px; } table {margin-top: 100px;}</style>
 			</head>
 			<body>
 				<form action="/" method="post">
 					<table width="400" align="center">
-						<tr><td class="success" align="center">������ ������ �� ��� email.</td></tr>
-						<tr><td align="center"><a href="/">�� �������</a></td></tr>
+						<tr><td class="success" align="center">Операция завершена!</td></tr>
+						<tr><td align="center"><a href="/">На главную</a></td></tr>
 					</table>
 				</form>
 			</body>
@@ -45,13 +47,13 @@ if (isset($_POST["lg"]) && !empty($_POST["lg"])):
 				<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap.css" />
 				<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap-theme.css" />
 				<link type="text/css" rel="stylesheet" href="/handle-style.css" />
-				<title>�����-������</title>
+				<title>Сброс пароля</title>
 				<style>td { height: 40px; } table {margin-top: 100px;}</style>
 			</head>
 			<body>
 					<table width="400" align="center">
-						<tr><td class="danger" align="center">������������ � ����� email �� ����������.</td></tr>
-						<tr><td align="center"><a href="/pass_reset/">���������</a></td></tr>
+						<tr><td class="danger" align="center">Укажите ваш email.</td></tr>
+						<tr><td align="center"><a href="/pass_reset/">Продолжить</a></td></tr>
 					</table>
 			</body>
 		</html>
@@ -65,15 +67,15 @@ else:?>
 		<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap.css" />
 		<link type="text/css" rel="stylesheet" href="/bootstrap-3.1.1-dist/css/bootstrap-theme.css" />
 		<link type="text/css" rel="stylesheet" href="/handle-style.css" />
-		<title>�����-������</title>
+		<title>Сброс пароля</title>
 		<style>td { height: 40px; } table {margin-top: 100px;}</style>
 	</head>
 	<body>
 		<form action="/pass_reset/" method="post">
 			<table width="300" align="center">
-				<tr><td colspan="2" align="center">�������������� ������</td></tr>
+				<tr><td colspan="2" align="center">Укажите ваш e-mail</td></tr>
 				<tr><td><label for="lg">E-mail:</label></td><td><input type="text" id="lg" name="lg" class="form-control" /></td></tr>
-				<tr><td colspan="2" align="center"><input type="submit" value="�����������" class="btn btn-default" /></td></tr>
+				<tr><td colspan="2" align="center"><input type="submit" value="admin" class="btn btn-default" /></td></tr>
 			</table>
 		</form>
 	</body>
